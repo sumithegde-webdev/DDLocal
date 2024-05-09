28f8004d1319e8a20264b71032330e7121fe9c44
Nikunj <nikunjthbs@gmail.com>
2024-05-09 14:26:35 +0530

    color changed

--- dd-front-end/src/components/Dashboard/Dashboard.jsx
+++ dd-front-end/src/components/Dashboard/Dashboard.jsx
@@ -221,7 +221,7 @@ const Dashboard = () => {
                                 </svg>
                             </div>
                         </div>
-                        <button class="ml-4 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" onClick={handleSearchButtonClick} >
+                        <button class="ml-4 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white  hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" onClick={handleSearchButtonClick}   style={{backgroundColor: "#2461FF"}}>
                             Search
                         </button>
                     </div>
@@ -243,7 +243,7 @@ const Dashboard = () => {
                                     {/* <p class="mt-2 text-sm text-gray-500">{product.userId}</p> */}
                                 </div>
                                 <div class="bg-gray-50 px-4 py-3 sm:px-6">
-                                    <button class="w-full inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                    <button class="w-full inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white  hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"   style={{backgroundColor: "#2461FF"}}>
                                         View Product
                                     </button>
                                 </div>
