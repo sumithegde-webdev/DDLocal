283436c834574cccd04af8749aaf2a1810e51895
Your Name <your.email@example.com>
2024-05-09 17:52:11 +0530

    Changes done

---
+++ dd-front-end/src/components/ForgotPassword/ForgotPassword.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import axios from "axios";
+
+const ForgotPassword = (props) => {
+
+    const [email, setFpEmail] = useState("");
+    const [error, setError] = useState({});
+
+    function emailChangeHandler(e) {
+        e.preventDefault();
+        setFpEmail(e.target.value);
+        // console.log(fpEmail);
+    }
+
+    function requestOTP() {
+
+        const emailValidationError = {}
+
+        const emailPattern = /^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+[.][A-Za-z]{2,4}$/;
+        if (!email.trim()) {
+            emailValidationError.emailError = "Email is required.";
+        }
+        else if (!emailPattern.test(email)) {
+            emailValidationError.emailError = "Please enter a valid Email. The format is _@_._";
+        }
+
+        setError(emailValidationError)
+        setTimeout(() => {
+            setError({});
+        }, 3000);
+
+        if (Object.keys(emailValidationError).length === 0) {
+            axios.post('http://localhost:8090/api/forgotpassword', {}, {
+                headers: {
+                    email: email,
+                    role: "user"
+                }
+            })
+                .then(() => {
+                    //change state here
+                    props.setEmail(email)
+                    props.setStage("verifyotp");
+                })
+                .catch((err) => {
+                    console.error(err);
+                });
+        }
+    }
+    return (
+        <>
+            <div id='fp--email--div' className='current--div'>
+                <div id='fp--title'>Forgot Password</div>
+                {/* <label htmlFor='fp--email--input'>Enter your Email address</label> */}
+                <input
+                    id='fp--email--input'
+                    className={error.emailError && "notvalid"}
+                    type='email'
+                    name='email'
+                    value={email}
+                    onChange={(e) => { emailChangeHandler(e) }}
+                    placeholder='Please enter the Registered Email'
+                />
+                {error.emailError && <div id='email--error--div'>{error.emailError}</div>}
+                <div id='fp--info--div'>An OTP will be sent to this Email address.<br></br>On successful OTP verification,<br></br> you can Reset your Password.</div>
+                <div id='otp--request--div'>
+                    <button id='otp--request--button' type='button' onClick={requestOTP}>Request OTP</button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ForgotPassword
---
+++ dd-front-end/src/components/ForgotPassword/ForgotPasswordSequence.jsx
@@ -0,0 +1,37 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import './forgotPassword.css'
+import ForgotPassword from './ForgotPassword'
+import VerifyOTP from './VerifyOTP'
+import ResetPassword from './ResetPassword'
+
+const ForgotPasswordSequence = () => {
+    const [stage, setStage] = useState("forgotpassword");
+
+    const [fpEmail, setfpEmail] = useState("");
+
+    useEffect(() => { ForgotPasswordSequence }, [stage]);
+
+    const stageHandler = (val) => {
+        setStage(val);
+    }
+
+    return (
+        <>
+            {
+                (stage === "forgotpassword") &&
+                <ForgotPassword setStage={stageHandler} setEmail={setfpEmail} />
+            }
+            {
+                (stage === "verifyotp") &&
+                <VerifyOTP setStage={stageHandler} userEmail={fpEmail} />
+            }
+            {
+                (stage === "resetpassword") &&
+                <ResetPassword setStage={stageHandler} userEmail={fpEmail} />
+            }
+        </>
+    )
+}
+
+export default ForgotPasswordSequence
